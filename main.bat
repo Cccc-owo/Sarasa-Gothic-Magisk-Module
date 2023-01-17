1ff0f60372b2fb0096b31d5613541434563de74d
Cccc-owo <net4hhhh@163.com>
2023-01-17 13:50:05 +0800

    chore: bump to v1.0.5
    - make Quotes(“”) in Latin/Greek/Cyrillic character set narrow

--- main.bat
+++ main.bat
@@ -11,29 +11,29 @@ if exist fonts\ del /F /S /Q fonts\ & rmdir /S /Q fonts\
 ECHO.
 wget.exe -O "font.7z" %font_zip%
 wget.exe -O "Template.zip" %template_zip%
-7z.exe x font.7z sarasa-gothic-sc-*.ttf -r -o.\fonts
+7z.exe x font.7z sarasa-ui-sc-*.ttf -r -o.\fonts
 7z.exe x Template.zip -r -o.\output
-xcopy fonts\sarasa-gothic-sc-*.ttf output\system\fonts /E /Q /C /Y
+xcopy fonts\sarasa-ui-sc-*.ttf output\system\fonts /E /Q /C /Y
 
-ren output\system\fonts\sarasa-gothic-sc-extralight.ttf fontchw2.ttf
-ren output\system\fonts\sarasa-gothic-sc-light.ttf fontchw3.ttf
-ren output\system\fonts\sarasa-gothic-sc-regular.ttf fontchw4.ttf
+ren output\system\fonts\sarasa-ui-sc-extralight.ttf fontchw2.ttf
+ren output\system\fonts\sarasa-ui-sc-light.ttf fontchw3.ttf
+ren output\system\fonts\sarasa-ui-sc-regular.ttf fontchw4.ttf
 copy output\system\fonts\fontchw4.ttf /Y output\system\fonts\fontchw5.ttf
-ren output\system\fonts\sarasa-gothic-sc-semibold.ttf fontchw6.ttf
-ren output\system\fonts\sarasa-gothic-sc-bold.ttf fontchw7.ttf
+ren output\system\fonts\sarasa-ui-sc-semibold.ttf fontchw6.ttf
+ren output\system\fonts\sarasa-ui-sc-bold.ttf fontchw7.ttf
 
-del /F /S /Q output\system\fonts\sarasa-gothic-sc-extralightitalic.ttf
-del /F /S /Q output\system\fonts\sarasa-gothic-sc-lightitalic.ttf
-del /F /S /Q output\system\fonts\sarasa-gothic-sc-italic.ttf
-del /F /S /Q output\system\fonts\sarasa-gothic-sc-semibolditalic.ttf
-del /F /S /Q output\system\fonts\sarasa-gothic-sc-bolditalic.ttf
+del /F /S /Q output\system\fonts\sarasa-ui-sc-extralightitalic.ttf
+del /F /S /Q output\system\fonts\sarasa-ui-sc-lightitalic.ttf
+del /F /S /Q output\system\fonts\sarasa-ui-sc-italic.ttf
+del /F /S /Q output\system\fonts\sarasa-ui-sc-semibolditalic.ttf
+del /F /S /Q output\system\fonts\sarasa-ui-sc-bolditalic.ttf
 
-::ren output\system\fonts\sarasa-gothic-sc-extralightitalic.ttf fontei2.ttf
-::ren output\system\fonts\sarasa-gothic-sc-lightitalic.ttf fontei3.ttf
-::ren output\system\fonts\sarasa-gothic-sc-italic.ttf fontei4.ttf
+::ren output\system\fonts\sarasa-ui-sc-extralightitalic.ttf fontei2.ttf
+::ren output\system\fonts\sarasa-ui-sc-lightitalic.ttf fontei3.ttf
+::ren output\system\fonts\sarasa-ui-sc-italic.ttf fontei4.ttf
 ::copy output\system\fonts\fontei4.ttf /Y output\system\fonts\fontei5.ttf
-::ren output\system\fonts\sarasa-gothic-sc-semibolditalic.ttf fontei6.ttf
-::ren output\system\fonts\sarasa-gothic-sc-bolditalic.ttf fontei7.ttf
+::ren output\system\fonts\sarasa-ui-sc-semibolditalic.ttf fontei6.ttf
+::ren output\system\fonts\sarasa-ui-sc-bolditalic.ttf fontei7.ttf
 
 del /F /S /Q output\module.prop
 xcopy module.prop output\
