a14906a359ab5277967dbec0e78e6e51c3113866
Cccc-owo <net4hhhh@163.com>
2023-11-26 14:01:26 +0800

    更新字体及脚本

--- main.bat
+++ main.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-set font_zip=https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.42.6/sarasa-gothic-ttf-0.42.6.7z
+set font_zip=https://github.com/be5invis/Sarasa-Gothic/releases/download/v1.0.0-alpha.2/Sarasa-TTF-1.0.0-alpha.2.7z
 set template_zip=https://github.com/lxgw/advanced-cjk-font-magisk-module-template/releases/download/v3.3.220605/FontTemplateA-Magisk204.zip
 ::checking
 ECHO Time:%date%
@@ -11,29 +11,29 @@ if exist fonts\ del /F /S /Q fonts\ & rmdir /S /Q fonts\
 ECHO.
 wget.exe -O "font.7z" %font_zip%
 wget.exe -O "Template.zip" %template_zip%
-7z.exe x font.7z sarasa-ui-sc-*.ttf -r -o.\fonts
+7z.exe x font.7z SarasaUiSC-*.ttf -r -o.\fonts
 7z.exe x Template.zip -r -o.\output
-xcopy fonts\sarasa-ui-sc-*.ttf output\system\fonts /E /Q /C /Y
+xcopy fonts\SarasaUiSC-*.ttf output\system\fonts /E /Q /C /Y
 
-ren output\system\fonts\sarasa-ui-sc-extralight.ttf fontchw2.ttf
-ren output\system\fonts\sarasa-ui-sc-light.ttf fontchw3.ttf
-ren output\system\fonts\sarasa-ui-sc-regular.ttf fontchw4.ttf
+ren output\system\fonts\SarasaUiSC-ExtraLight.ttf fontchw2.ttf
+ren output\system\fonts\SarasaUiSC-Light.ttf fontchw3.ttf
+ren output\system\fonts\SarasaUiSC-Regular.ttf fontchw4.ttf
 copy output\system\fonts\fontchw4.ttf /Y output\system\fonts\fontchw5.ttf
-ren output\system\fonts\sarasa-ui-sc-semibold.ttf fontchw6.ttf
-ren output\system\fonts\sarasa-ui-sc-bold.ttf fontchw7.ttf
+ren output\system\fonts\SarasaUiSC-SemiNold.ttf fontchw6.ttf
+ren output\system\fonts\SarasaUiSC-Bold.ttf fontchw7.ttf
 
-del /F /S /Q output\system\fonts\sarasa-ui-sc-extralightitalic.ttf
-del /F /S /Q output\system\fonts\sarasa-ui-sc-lightitalic.ttf
-del /F /S /Q output\system\fonts\sarasa-ui-sc-italic.ttf
-del /F /S /Q output\system\fonts\sarasa-ui-sc-semibolditalic.ttf
-del /F /S /Q output\system\fonts\sarasa-ui-sc-bolditalic.ttf
+del /F /S /Q output\system\fonts\SarasaUiSC-ExtraLightItalic.ttf
+del /F /S /Q output\system\fonts\SarasaUiSC-LightItalic.ttf
+del /F /S /Q output\system\fonts\SarasaUiSC-Italic.ttf
+del /F /S /Q output\system\fonts\SarasaUiSC-SemiNoldItalic.ttf
+del /F /S /Q output\system\fonts\SarasaUiSC-BoldItalic.ttf
 
-::ren output\system\fonts\sarasa-ui-sc-extralightitalic.ttf fontei2.ttf
-::ren output\system\fonts\sarasa-ui-sc-lightitalic.ttf fontei3.ttf
-::ren output\system\fonts\sarasa-ui-sc-italic.ttf fontei4.ttf
+::ren output\system\fonts\SarasaUiSC-ExtraLightItalic.ttf fontei2.ttf
+::ren output\system\fonts\SarasaUiSC-LightItalic.ttf fontei3.ttf
+::ren output\system\fonts\SarasaUiSC-Italic.ttf fontei4.ttf
 ::copy output\system\fonts\fontei4.ttf /Y output\system\fonts\fontei5.ttf
-::ren output\system\fonts\sarasa-ui-sc-semibolditalic.ttf fontei6.ttf
-::ren output\system\fonts\sarasa-ui-sc-bolditalic.ttf fontei7.ttf
+::ren output\system\fonts\SarasaUiSC-SemiNoldItalic.ttf fontei6.ttf
+::ren output\system\fonts\SarasaUiSC-BoldItalic.ttf fontei7.ttf
 
 del /F /S /Q output\module.prop
 xcopy module.prop output\
