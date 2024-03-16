3ef38c51d1ca3af23137c45120904f9b4a3d21f6
Cccc-owo <net4hhhh@163.com>
2024-03-16 21:42:52 +0800

    v1.2.9 - 更新更纱黑体：```v1.0.5``` => ```v1.0.6```

--- main.bat
+++ main.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-set font_zip=https://github.com/be5invis/Sarasa-Gothic/releases/download/v1.0.5/Sarasa-TTF-Unhinted-1.0.5.7z
+set font_zip=https://github.com/be5invis/Sarasa-Gothic/releases/download/v1.0.6b/SarasaGothicSC-TTF-1.0.6.zip
 set template_zip=https://github.com/lxgw/advanced-cjk-font-magisk-module-template/releases/download/v3.3.220605/FontTemplateA-Magisk204.zip
 ::checking
 ECHO Time:%date%
@@ -11,29 +11,29 @@ if exist fonts\ del /F /S /Q fonts\ & rmdir /S /Q fonts\
 ECHO.
 wget.exe -O "font.7z" %font_zip%
 wget.exe -O "Template.zip" %template_zip%
-7z.exe x font.7z SarasaUiSC-*.ttf -r -o.\fonts
+7z.exe x font.7z SarasaGothicSC-*.ttf -r -o.\fonts
 7z.exe x Template.zip -r -o.\output
-xcopy fonts\SarasaUiSC-*.ttf output\system\fonts /E /Q /C /Y
+xcopy fonts\SarasaGothicSC-*.ttf output\system\fonts /E /Q /C /Y
 
-ren output\system\fonts\SarasaUiSC-ExtraLight.ttf fontchw2.ttf
-ren output\system\fonts\SarasaUiSC-Light.ttf fontchw3.ttf
-ren output\system\fonts\SarasaUiSC-Regular.ttf fontchw4.ttf
+ren output\system\fonts\SarasaGothicSC-ExtraLight.ttf fontchw2.ttf
+ren output\system\fonts\SarasaGothicSC-Light.ttf fontchw3.ttf
+ren output\system\fonts\SarasaGothicSC-Regular.ttf fontchw4.ttf
 copy output\system\fonts\fontchw4.ttf /Y output\system\fonts\fontchw5.ttf
-ren output\system\fonts\SarasaUiSC-SemiBold.ttf fontchw6.ttf
-ren output\system\fonts\SarasaUiSC-Bold.ttf fontchw7.ttf
+ren output\system\fonts\SarasaGothicSC-SemiBold.ttf fontchw6.ttf
+ren output\system\fonts\SarasaGothicSC-Bold.ttf fontchw7.ttf
 
-del /F /S /Q output\system\fonts\SarasaUiSC-ExtraLightItalic.ttf
-del /F /S /Q output\system\fonts\SarasaUiSC-LightItalic.ttf
-del /F /S /Q output\system\fonts\SarasaUiSC-Italic.ttf
-del /F /S /Q output\system\fonts\SarasaUiSC-SemiBoldItalic.ttf
-del /F /S /Q output\system\fonts\SarasaUiSC-BoldItalic.ttf
+del /F /S /Q output\system\fonts\SarasaGothicSC-ExtraLightItalic.ttf
+del /F /S /Q output\system\fonts\SarasaGothicSC-LightItalic.ttf
+del /F /S /Q output\system\fonts\SarasaGothicSC-Italic.ttf
+del /F /S /Q output\system\fonts\SarasaGothicSC-SemiBoldItalic.ttf
+del /F /S /Q output\system\fonts\SarasaGothicSC-BoldItalic.ttf
 
-::ren output\system\fonts\SarasaUiSC-ExtraLightItalic.ttf fontei2.ttf
-::ren output\system\fonts\SarasaUiSC-LightItalic.ttf fontei3.ttf
-::ren output\system\fonts\SarasaUiSC-Italic.ttf fontei4.ttf
+::ren output\system\fonts\SarasaGothicSC-ExtraLightItalic.ttf fontei2.ttf
+::ren output\system\fonts\SarasaGothicSC-LightItalic.ttf fontei3.ttf
+::ren output\system\fonts\SarasaGothicSC-Italic.ttf fontei4.ttf
 ::copy output\system\fonts\fontei4.ttf /Y output\system\fonts\fontei5.ttf
-::ren output\system\fonts\SarasaUiSC-SemiBoldItalic.ttf fontei6.ttf
-::ren output\system\fonts\SarasaUiSC-BoldItalic.ttf fontei7.ttf
+::ren output\system\fonts\SarasaGothicSC-SemiBoldItalic.ttf fontei6.ttf
+::ren output\system\fonts\SarasaGothicSC-BoldItalic.ttf fontei7.ttf
 
 del /F /S /Q output\module.prop
 xcopy module.prop output\
